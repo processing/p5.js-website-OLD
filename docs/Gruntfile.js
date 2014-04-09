7f46acb53c8f405337e9bcd96983c9c0a1027ee7
lauren mccarthy <laurmccarthy@gmail.com>
2014-04-09 11:04:52 -0400

    test

--- docs/Gruntfile.js
+++ docs/Gruntfile.js
@@ -11,6 +11,7 @@ module.exports = function(grunt) {
         url: '<%= pkg.homepage %>',
         options: {
           paths: '../src/',
+          themedir: 'yuidoc-p5-theme/',
           //helpers: [],
           outdir: 'api/'
         }
--- reference/api.js
+++ reference/api.js
@@ -1,7 +1,7 @@
 YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
-        "PVecotr"
+        "PVector"
     ],
     "modules": [
         "PVector"
