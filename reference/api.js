a3b32a71b0a097e0dfa0f46f4ae5616c94efa8a6
lauren mccarthy <laurmccarthy@gmail.com>
2014-04-09 10:57:25 -0400

    test

---
+++ reference/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "PVecotr"
+    ],
+    "modules": [
+        "PVector"
+    ],
+    "allModules": [
+        {
+            "displayName": "PVector",
+            "name": "PVector"
+        }
+    ]
+} };
+});
