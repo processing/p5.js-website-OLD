84de7f4d62bbef661abd0a7f4e65a914a89b0007
lauren mccarthy <laurmccarthy@gmail.com>
2014-04-09 10:55:55 -0400

    testing docs

--- docs/Gruntfile.js
+++ docs/Gruntfile.js
@@ -1,34 +1,8 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('../package.json'),
-    sass: {
-      dist: {
-        files: {
-          // 'destination': 'source'
-          'yuidoc-p5-theme/assets/css/main.css': 'yuidoc-p5-theme-src/sass/main.scss'
-        }
-      }
-    },
-    watch: {
-      sass: {
-        files: ['yuidoc-p5-theme-src/sass/**/*.scss'],
-        tasks: ['sass']
-      },
-//      src: {
-//        files: ['yuidoc-p5-theme/assets/js/**/*.js'],
-//        tasks: ['requirejs']
-//      },
-      docs: {
-        files: [
-          'yuidoc-p5-theme/assets/js/**/*.js', 
-          'yuidoc-p5-theme/partials/*.handlebars', 
-          'yuidoc-p5-theme/layouts/*.handlebars',
-          'yuidoc-p5-theme/assets/js/tpl/**/*'
-        ],
-        tasks: ['yuidoc']
-      }
-    },
+    pkg: grunt.file.readJSON('package.json'),
+
     yuidoc: {
       compile: {
         name: '<%= pkg.name %>',
@@ -37,47 +11,20 @@ module.exports = function(grunt) {
         url: '<%= pkg.homepage %>',
         options: {
           paths: '../src/',
-          themedir: 'yuidoc-p5-theme/',
           //helpers: [],
           outdir: 'api/'
         }
       }
     }
-//    ,
-//    requirejs: {
-//      unmin: {
-//        options: {
-//          baseUrl: '.',
-//          findNestedDependencies: true,
-//          include: ['src/p5'],
-//          optimize: 'none',
-//          out: 'dist/p5.js',
-//          useStrict: true,
-//          wrap: true
-//        }
-//      },
-//      min: {
-//        options: {
-//          baseUrl: '.',
-//          findNestedDependencies: true,
-//          include: ['src/p5'],
-//          optimize: 'uglify2',
-//          out: 'dist/p5.min.js',
-//          paths: '<%= requirejs.unmin.options.paths %>',
-//          useStrict: true,
-//          wrap: true
-//        }
-//      }
-//    }
 
   });
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
+//  grunt.loadNpmTasks('grunt-contrib-watch');
 //  grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-yuidoc');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+//  grunt.loadNpmTasks('grunt-contrib-sass');
 
-  grunt.registerTask('yui', ['yuidoc']);
-  //grunt.registerTask('default', ['sass', 'yuidoc']);
+ // grunt.registerTask('yui', ['yuidoc']);
+  grunt.registerTask('default', [ 'yuidoc']);
 
 };
