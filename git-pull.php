c4f384ad8d8046e7f77ef62163bab58355b49f61
lauren mccarthy <laurmccarthy@gmail.com>
2014-07-06 18:33:41 -0400

    date

--- git-pull.php
+++ git-pull.php
@@ -1,5 +1,17 @@
 <?php
+echo system('git reset --hard');
 echo system('git checkout -- .');
 echo system('git pull git://github.com/lmccart/p5js.org.git master');
-echo system('"test" > updated.php');
+
+$date = getdate();
+$date_str = $date['month'].' '.$date['mday'].', '.$date['year'];
+
+$f = fopen('updated.php', 'w');
+fwrite($f, $date_str);
+fclose($f); 
+
+
+$f = fopen('site/updated.php', 'w');
+fwrite($f, $date_str);
+fclose($f); 
 ?>
