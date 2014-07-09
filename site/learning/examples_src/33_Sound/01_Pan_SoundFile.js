e7bb0c885a62735942e09a802dd22e55e5cbd529
Jason Sigal <jason.sigal@gmail.com>
2014-07-09 17:42:31 -0400

    adding sound examples and p5.sound and p5.dom

---
+++ site/learning/examples_src/33_Sound/01_Pan_SoundFile.js
@@ -0,0 +1,32 @@
+/*
+ * @name Pan Sound
+ * @description Ball position follows mouse and correlates to panning of sound.
+ * Click mouse to play
+ */
+
+var ball = {};
+var soundFile;
+var panning;
+
+function preload() {
+  soundFile = loadSound( ['assets/beatbox.ogg', 'assets/beatbox.wav'] );
+}
+
+function setup() {
+  createCanvas(700, 100);
+}
+
+function draw() {
+  background(0, 0, 0);
+  ball.x = constrain(mouseX, 0, width);
+  ellipse(ball.x, height/2, 100, 100)
+
+  // map the ball's x location to a panning degree (float between -1.0 and 1.0)
+  panning = map(ball.x, 0., width,-1.0, 1.0);
+}
+
+function mouseClicked(){
+  soundFile.pan(panning);
+  // play the sound
+  soundFile.play();
+}
---
+++ site/learning/examples_src/33_Sound/02_Oscillator_FFT.js
@@ -0,0 +1,25 @@
+/*
+ * @name Oscillator
+ * @description Control an Oscillator and view the waveform using FFT
+ */
+var osc, fft;
+
+function setup() {
+  createCanvas(800,400);
+  osc = new Oscillator(220, 'triangle'); // set frequency and type
+  osc.amp(.5);
+  fft = new FFT();
+  osc.start();
+}
+
+function draw() {
+  background(255);
+  waveform = fft.waveform();  // analyze the waveform
+  beginShape();
+  for (var i = 0; i < waveform.length; i++){
+    var x = map(i, 0, waveform.length, 0, width);
+    var y = map(waveform[i], 0, 256, -height/2, height/2);
+    vertex(x, y + height/2);
+  }
+  endShape();
+}
