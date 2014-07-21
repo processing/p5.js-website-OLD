d4bff4e196f9f59fc0cb12ee435fcda2af16c6b4
lauren mccarthy <laurmccarthy@gmail.com>
2014-07-21 15:45:22 -0400

    adding search to head bar

--- site/js/main.js
+++ site/js/main.js
@@ -19,3 +19,27 @@ $(window).scroll(function(){
     $('#menu').css({'margin-top': 0});
   }    
 });
+
+
+$('#family form').hover(
+  function() {
+    $('#search_field').css('visibility', 'visible');
+    $('#search_field').animate({width: '9.75em'}, 100);
+  },
+  function() {
+    if ($('#search_field').val() === '') {
+      closeSearch();
+    }
+  }
+);
+
+$('#family form').focusout(function() {
+  closeSearch();
+});
+
+function closeSearch() {
+
+  $('#search_field').css('width', '0em');
+  $('#search_field').css('visibility', 'hidden');
+  console.log($('#search_field').attr('width'));
+}
