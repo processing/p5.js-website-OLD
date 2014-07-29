58b54de5bb5a4b674e29eb4708d103b216b238bf
lauren mccarthy <laurmccarthy@gmail.com>
2014-07-28 22:21:46 -0400

    dl page

--- site/download/index.php
+++ site/download/index.php
@@ -10,18 +10,44 @@
     <div class="column-span">
       <section id="about">
         <h2>Download</h2>
-        <a href="https://github.com/lmccart/p5.js/releases/download/<?php echo $version; ?>/p5.js.zip"><h3>> 
-        Download p5.js complete</h3></a>
-        <p>Includes <code>p5.js</code>, <a href="../reference/#/libraries/dom"><code>p5.dom.js</code></a>, 
-        <a href="../reference/#/libraries/sound"><code>p5.sound.js</code></a>, and an empty example.
-        <br>Version <?php echo $version; ?> (<?php echo $date; ?>)</p>
-        <a href="#" class="anchor"><h3>Single Files</h3></a>
-        <ul>
-          <li><a href="https://github.com/lmccart/p5.js/releases/download/<?php echo $version; ?>/p5.js">Download p5.js</a> - Full, uncompressed version.</li>
-          <li><a href="https://github.com/lmccart/p5.js/releases/download/<?php echo $version; ?>/p5.min.js">Download p5.min.js</a> - Compressed version.</li>
-          <li><a href="http://cdnjs.com/libraries/p5.js">CDN</a> - Link to statically hosted file.</li>
-        </ul>
+        <h3>Complete Library</h3>
 
+        <a href="https://github.com/lmccart/p5.js/releases/download/<?php echo $version; ?>/p5.js.zip">
+        <div class="download_box">
+        <h4>p5.js complete</h4>
+        <object type="image/svg+xml" data="../img/thick-asterisk-alone.svg">
+         *<!-- to do: add fallback image in CSS -->
+        </object>
+        <p>Includes:<br>p5.js, p5.dom.js, p5.sound.js, and an example project
+        <br>Version <?php echo $version; ?> (<?php echo $date; ?>)</p>
+        </div>
+        </a>
+
+
+        <div>
+        <h3>Single Files</h3>
+        <a href="https://github.com/lmccart/p5.js/releases/download/<?php echo $version; ?>/p5.js">
+        <div class="download_box half_box">
+        <h4>p5.js</h4>
+        <p>Single file:<br>Full uncompressed version</p>
+        </div>
+        </a>
+
+        <a href="https://github.com/lmccart/p5.js/releases/download/<?php echo $version; ?>/p5.min.js">
+        <div class="download_box half_box">
+        <h4>p5.min.js</h4>
+        <p>Single file:<br>Compressed version</p>
+        </div>
+        </a>
+
+        <a href="http://cdnjs.com/libraries/p5.js">
+        <div class="download_box half_box">
+        <h4>CDN</h4>
+        <p>Link:<br>Statically hosted file</p>
+        </div>
+        </a>
+
+        </div>
 
         
       </section>
