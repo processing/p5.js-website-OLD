d4e53fc7172f418091bc02871b002e3b20d802ff
lauren mccarthy <laurmccarthy@gmail.com>
2014-07-30 08:08:15 -0400

    learn page

--- site/learn/index.php
+++ site/learn/index.php
@@ -11,49 +11,47 @@
     <section>
       <h2>Learn</h2>
 
-
       <a name="tutorials" class="anchor"><h3>Tutorials</h3></a>
+
       <div class="left-column">
-        <img src="../img/test-image-1.jpg">
-        <p><a href="https://github.com/lmccart/p5.js/wiki/Getting-Started">Getting
-        started</a><br>Set up your environment and create your first sketch.</p>
-      </div>
-      <div class="right-column">
-        <img src="../img/test-image-1.jpg">
-        <p><a href="https://github.com/lmccart/p5.js/wiki/Processing-syntax-conversion">
-        Processing syntax conversion</a><br>Processing > p5.js and back!</p> 
+        <a class="nounderline" href="<?php echo getRoot(); ?>reference/#/libraries/p5.dom">
+        <div class="label">
+        <img src="../img/learn/lib_placeholder.jpg"></a>
+        <a class="nounderline" href="<?php echo getRoot(); ?>reference/#/libraries/p5.dom"><h4>Beyond the canvas</h4></a>
+        </div>
+        <p>Creating and manipulating elements on the page 
+        beyond the canvas.</p> 
       </div>
 
+      <div class="right-column">
+        <a class="nounderline" href="<?php echo getRoot(); ?>reference/#/libraries/p5.sound">
+        <div class="label">
+        <img src="../img/learn/lib_placeholder.jpg"></a>
+        <a class="nounderline" href="<?php echo getRoot(); ?>reference/#/libraries/p5.sound"><h4>Including other libraries</h4></a>
+        </div>
+        <p>Finding, adding, and integrating other JS libraries.</p> 
+      </div>
       <div class="spacer"></div>
 
       <div class="left-column">
-        <img src="../img/test-image-1.jpg">
-        <p><a href="https://github.com/lmccart/p5.js/wiki/DOM-Extensions">
-        Beyond the canvas</a><br>Creating and manipulating elements on the page 
-        beyond the canvas.</p>
-      </div>
-      <div class="right-column">
-        <img src="../img/test-image-1.jpg">
-        <p><a href="https://github.com/lmccart/p5.js/wiki/Integrating-other-libraries">
-        Working with other libraries</a><br>Finding, adding, and integrating 
-        other JS libraries.</p> 
-      </div>
-
-      <div class="spacer"></div>
-
-      <div class="left-column">
-        <img src="../img/test-image-1.jpg">
-        <p><a href="https://github.com/lmccart/p5.js/wiki/Local-server">
-        Enabling a local server</a><br>Some functionality (loading external 
+        <a class="nounderline" href="<?php echo getRoot(); ?>reference/#/libraries/p5.dom">
+        <div class="label">
+        <img src="../img/learn/lib_placeholder.jpg"></a>
+        <a class="nounderline" href="<?php echo getRoot(); ?>reference/#/libraries/p5.dom"><h4>Using a local server</h4></a>
+        </div>
+        <p>Some functionality (loading external 
         files, for example) requires the use of a local server for local 
         development. This tutorial includes instructions for setting this up on
         Mac OSX, Windows, and Linux.</p> 
       </div>
+
       <div class="right-column">
-        <img src="../img/test-image-1.jpg">
-        <p><a href="https://github.com/lmccart/p5.js/wiki/Development">
-        Development</a><br>Getting started and overview for those that want to
-        contribute to development.</p> 
+        <a class="nounderline" href="<?php echo getRoot(); ?>reference/#/libraries/p5.sound">
+        <div class="label">
+        <img src="../img/learn/lib_placeholder.jpg"></a>
+        <a class="nounderline" href="<?php echo getRoot(); ?>reference/#/libraries/p5.sound"><h4>Development</h4></a>
+        </div>
+        <p>Getting started and overview for those that want to contribute to development.</p> 
       </div>
 
       <div class="spacer"></div>
