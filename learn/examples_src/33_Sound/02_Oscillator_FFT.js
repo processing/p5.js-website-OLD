a4ef1eebcb17e72dd82b9c6f0f19562840b0d0e9
Jason Sigal <jason.sigal@gmail.com>
2014-08-06 11:27:04 -0400

    adding files

--- learn/examples_src/33_Sound/02_Oscillator_FFT.js
+++ learn/examples_src/33_Sound/02_Oscillator_FFT.js
@@ -2,23 +2,31 @@
  * @name Oscillator
  * @description Control an Oscillator and view the waveform using FFT
  */
-var osc, fft;
+var osc, fft, soundFile;
+
+function preload() {
+  var soundFile = loadSound( ['assets/beatbox.ogg', 
+                          'assets/beatbox.wav'] );
+}
 
 function setup() {
   createCanvas(720, 400);
-  osc = new Oscillator(220, 'triangle'); // set frequency and type
+  background(0);
+  soundFile.play();
+  osc = new p5.SinOsc(); // set frequency and type
   osc.amp(.5);
-  fft = new FFT();
+  fft = new p5.FFT();
   osc.start();
 }
 
 function draw() {
-  background(255);
-  waveform = fft.waveform();  // analyze the waveform
+  background(255,255,0);
+  var waveform = fft.waveform();  // analyze the waveform
   beginShape();
+  fill(0);
   for (var i = 0; i < waveform.length; i++){
     var x = map(i, 0, waveform.length, 0, width);
-    var y = map(waveform[i], 0, 256, -height/2, height/2);
+    var y = map(waveform[i], 0, 256, height, 0);
     vertex(x, y + height/2);
   }
   endShape();
