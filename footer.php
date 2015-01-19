6697eb03e0d52c9e75e678e130fcb68be47fd428
Lauren McCarthy <laurmccarthy@gmail.com>
2015-01-19 18:38:56 -0500

    adding noisewave preview

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
   <p>p5.js is developed by <a href="http://lauren-mccarthy.com" target="_blank">Lauren McCarthy</a> 
-  and <a href="<?php echo getRoot(); ?>contribute/#contributors">others</a>, 
-  with the support of the <a href="http://processing.org/foundation/" target="_blank">Processing Foundation</a> 
+  and <a href="<?php echo getRoot(); ?>contribute/#contributors">collaborators</a>, 
+  with support from <a href="http://processing.org/foundation/" target="_blank">Processing Foundation</a> 
   and <a href="http://itp.nyu.edu/itp/" target="_blank">NYU ITP</a>.<br>
   <a href="<?php echo getRoot(); ?>copyright.php">&copy; Info.</a> Updated <?php include('updated.php'); ?>.</p>
 </footer>
