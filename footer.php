5406491b6e784e83a2709f28fb6e4e7b3c0e8ef1
Lauren McCarthy <laurmccarthy@gmail.com>
2015-03-20 18:00:52 +0000

    ref

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
 <footer>
-  <p>p5.js is developed by <a href="http://lauren-mccarthy.com" target="_blank">Lauren McCarthy</a> 
-  and <a href="<?php echo getRoot(); ?>contribute/#contributors">collaborators</a>, 
+  <p>p5.js is developed by a group of collaborators, 
   with support from <a href="http://processing.org/foundation/" target="_blank">Processing Foundation</a> 
   and <a href="http://itp.nyu.edu/itp/" target="_blank">NYU ITP</a>.<br>
   <a href="<?php echo getRoot(); ?>copyright.php">&copy; Info.</a> Updated <?php include('updated.php'); ?>.</p>
