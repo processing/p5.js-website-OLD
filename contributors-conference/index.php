4b6955c9cc520ebe238586b2253f2aa68d3398e9
Lauren McCarthy <laurmccarthy@gmail.com>
2015-04-30 20:53:06 -0400

    temp

---
+++ contributors-conference/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+
+  <?php include('../header.php'); ?>
+  <body id="reference-page">
+
+    <?php include('../sidebar.php'); ?>
+
+    <!-- content sections -->
+    <div class="column-span">
+
+    <section>
+
+      <h2>p5.js Contributors Conference</h2> 
+
+
+      <div id="contributors-subsection">
+      <h3>Frank-Ratchye STUDIO for Creative Inquiry<br>Carnegie Mellon University<br>May 25-31</h3>
+
+      <p>For one week, a diverse group of participants will gather to work on all aspects of p5.js, 
+      including (but not limited to) new features, bug fixing, documentation, outreach, community, 
+      design, and internationalization.
+      </p>
+      </div>
+
+      <div id="contributors-subsection">
+      <h3>Participants</h3>
+      <p>
+        <a href='http://huah.net/jason/' target='_blank'>Jason Alderman</a>,
+        <a href='http://sepans.com/' target='_blank'>Sepand Ansari</a>,
+        <a href='http://tegabrain.com' target='_blank'>Tega Brain</a>,
+        <a href='https://medium.com/@emchenNYC/' target='_blank'>Emily Chen</a>,
+        <a href='http://andrescolubri.net/' target='_blank'>Andres Colubri</a>,
+        <a href='https://twitter.com/lucapodular' target='_blank'>Luca Damasco</a>,
+        <a href='http://guydebree.com/' target='_blank'>Guy de Bree</a>,
+        <a href='http://www.cjdecarteret.com/' target='_blank'>Christine de Carteret</a>,
+        <a href='http://xystudio.cc/' target='_blank'>Xy Feng</a>,
+        <a href='http://www.sarahgp.com/' target='_blank'>Sarah Groff-Palermo</a>,
+        <a href='http://www.crhallberg.com/' target='_blank'>Chris Hallberg</a>,
+        <a href='http://valhead.com/' target='_blank'>Val Head</a>,
+        <a href='http://johannahedva.com' target='_blank'>Johanna Hedva</a>,
+        <a href='http://www.katehollenbach.com/' target='_blank'>Kate Hollenbach</a>,
+        <a href='http://web.media.mit.edu/~jacobsj/' target='_blank'>Jennifer Jacobs</a>,
+        <a href='http://www.epicjefferson.com/' target='_blank'>Epic Jefferson</a>,
+        <a href='#' target='_blank'>Eddy Man Kim</a>,
+        <a href='http://lav.io/' target='_blank'>Sam Lavigne</a>,
+        <a href='http://flong.com' target='_blank'>Golan Levin</a>,
+        <a href='http://www.liuchangitp.com/' target='_blank'>Cici Liu</a>,
+        <a href='http://flong.com' target='_blank'>Maya Man</a>,
+        <a href='http://lauren-mccarthy.com' target='_blank'>Lauren McCarthy</a>,
+        <a href='http://www.workergnome.com/' target='_blank'>David Newbury</a>,
+        <a href='http://molleindustria.org/' target='_blank'>Paolo Pedercini</a>,
+        <a href='http://luisaph.com' target='_blank'>Luisa Pereira</a>,
+        <a href='https://twitter.com/mileshiroo' target='_blank'>Miles Peyton</a>,
+        <a href='https://pibloginthesky.wordpress.com/' target='_blank'>Stephanie Pi</a>,
+        <a href='http://jasonsigal.cc' target='_blank'>Jason Sigal</a>,
+        <a href='http://studioindefinit.com/' target='_blank'>Kevin Siwoff</a>,
+        <a href='http://charlottestiles.com/' target='_blank'>Charlotte Stiles</a>,
+        <a href='http://www.danomatika.com/' target='_blank'>Dan Wilcox</a>
+      </p>
+      </div>
+
+      <div id="contributors-subsection">
+      <h3>Participate Remotely</h3>
+
+      <p>We welcome virtual participate from afar! To follow along, you can join the p5.js Contributors Conference 
+      <a href='https://groups.google.com/forum/#!forum/p5js-contributors-conference' target='_blank'>mailing list</a>
+      for updates, follow <a href='http://twitter.com/p5xjs' target='_blank'>@p5xjs</a> on Twitter, participate in conversation 
+      on <a href='https://github.com/processing/p5.js/issues' target='_blank'>Github</a>, or join the conversation on Freenode IRC 
+      in the <a href='http://webchat.freenode.net/?channels=p5js' target='_blank'>#p5js channel</a>.</li>
+      </p>
+
+      </div> 
+
+      <div id="contributors-subsection">
+      <h3>Support</h3>
+
+      <p>Our contributor conference will take place at the <a href='http://studioforcreativeinquiry.org'>Frank-Ratchye STUDIO for Creative Inquiry</a>
+      at Carnegie Mellon University, an academic laboratory for atypical, anti-disciplinary, and inter-institutional research at the intersections of arts, 
+      science, technology, and culture. This event is supported by a grant from the National Endowment for the Arts, with additional support from 
+      the <a href='http://processing.org'>Processing Foundation</a>, the <a href='http://itp.nyu.edu'>NYU Interactive Telecommunications Program</a>,
+      <a href='http://theartificial.nl/'>The Artificial</a>, <a href='http://www.du.edu/ahss/edp/'>University of Denver, Emergent Digital Practices</a>,
+      and <a href='http://tinysubversions.com/'>Darius Kazemi</a>.</p>
+      </div> 
+
+
+    </section>
+
+  
+    <?php include('../footer.php'); ?>
+    <!-- outside of column for footer to go across both -->
+
+    <p class="clearfix"> &nbsp; </p>
+
+    </div><!-- end id="container"  -->
+
+    <object type="image/svg+xml" data="../img/thick-asterisk-alone.svg" id="asterisk-design-element">
+         *<!-- to do: add fallback image in CSS -->
+    </object>
+
+
+    <?php include('../end.php'); ?>
+  </body>
+</html>
