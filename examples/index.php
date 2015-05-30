b340e9e12f8abff8e8a79a04946c61fbc1ab9149
Stephanie <stephmpi@gmail.com>
2015-05-29 21:46:37 -0400

    moved tutorials out of examples

--- examples/index.php
+++ examples/index.php
@@ -9,486 +9,404 @@
     <div class="column-span">
 
     <section>
-      <h2>Learn</h2>
 
-      <a name="tutorials" class="anchor"><h3>Tutorials</h3></a>
-      
-      <p>The <a href="#tutorials">tutorials</a> provide more in-depth or step-by-step overviews of 
-      particular topics. Scroll down to see <a href="#examples">examples</a> demonstrating a variety
-      of p5.js topics.</p>
-
-      <div class="left-column">
-        <a class="nounderline" href="https://github.com/processing/p5.js/wiki/p5.js-overview">
-        <div class="label">
-        <img src="../img/learn/lib_placeholder.jpg"></a>
-        <a class="nounderline" href="https://github.com/processing/p5.js/wiki/p5.js-overview"><h4>p5.js overview</h4></a>
-        </div>
-        <p>An overview of the main features of p5.js.</p> 
-      </div>
-      
-      <div class="right-column">
-        <a class="nounderline" href="https://github.com/processing/p5.js/wiki/Processing-transition">
-        <div class="label">
-        <img src="../img/learn/lib_placeholder.jpg"></a>
-        <a class="nounderline" href="https://github.com/processing/p5.js/wiki/Processing-transition"><h4>p5.js and Processing</h4></a>
-        </div>
-        <p>The main differences between the two, and how to convert from one to the other.</p> 
-      </div>
-
-
-      <div class="spacer"></div>
-
-      <div class="left-column">
-        <a class="nounderline" href="https://github.com/processing/p5.js/wiki/Beyond-the-canvas">
-        <div class="label">
-        <img src="../img/learn/dom.jpg"></a>
-        <a class="nounderline" href="https://github.com/processing/p5.js/wiki/Beyond-the-canvas"><h4>Beyond the canvas</h4></a>
-        </div>
-        <p>Creating and manipulating elements on the page 
-        beyond the canvas.</p> 
-      </div>
-
-      <div class="right-column">
-        <a class="nounderline" href="https://github.com/processing/p5.js/wiki/Integrating-other-libraries">
-        <div class="label">
-        <img src="../img/learn/lib_placeholder.jpg"></a>
-        <a class="nounderline" href="https://github.com/processing/p5.js/wiki/Integrating-other-libraries"><h4>Adding libraries</h4></a>
-        </div>
-        <p>Finding, adding, and integrating other JS libraries.</p> 
-      </div>
-
-
-      <div class="spacer"></div>
-
-      <div class="left-column">
-        <a class="nounderline" href="https://github.com/processing/p5.js/wiki/Local-server">
-        <div class="label">
-        <img src="../img/learn/lib_placeholder.jpg"></a>
-        <a class="nounderline" href="https://github.com/processing/p5.js/wiki/Local-server"><h4>Using a local server</h4></a>
-        </div>
-        <p>How to set up a local server on Mac OSX, Windows, or Linux.</p> 
-      </div>
-
-      <div class="right-column">
-        <a class="nounderline" href="https://github.com/processing/p5.js/wiki/p5.js,-node.js,-socket.io">
-        <div class="label">
-        <img src="../img/learn/lib_placeholder.jpg"></a>
-        <a class="nounderline" href="https://github.com/processing/p5.js/wiki/p5.js,-node.js,-socket.io"><h4>node.js and socket.io</h4></a>
-        </div>
-        <p>Using a node.js server with p5.js, communcation via socket.io.</p> 
-      </div>
-
-      <div class="spacer"></div>
-
-
-      <div class="left-column">
-        <a class="nounderline" href="https://github.com/processing/p5.js/wiki/Development">
-        <div class="label">
-        <img src="../img/learn/lib_placeholder.jpg"></a>
-        <a class="nounderline" href="https://github.com/processing/p5.js/wiki/Development"><h4>Development</h4></a>
-        </div>
-        <p>Getting started and overview for contributing to development.</p> 
-      </div>
-
-      <div class="spacer"></div>
-
-      <a name="examples" class="anchor"><h3>Examples</h3></a>
+      <a name="examples" class="anchor"><h2>Examples</h2></a>
       <div class="column_0">
-      
+
         <p>
         <strong>Structure</strong><br>
-        
+
           <a href="examples/Structure_Coordinates.php">Coordinates</a><br>
-          
-        
+
+
           <a href="examples/Structure_Width_and_Height.php">Width/Height</a><br>
-          
-        
+
+
           <a href="examples/Structure_Setup_and_Draw.php">Setup/Draw</a><br>
-          
-        
+
+
           <a href="examples/Structure_No_Loop.php">No Loop</a><br>
-          
-        
+
+
           <a href="examples/Structure_Loop.php">Loop</a><br>
-          
-        
+
+
         </p>
-        
-      
+
+
         <p>
         <strong>Form</strong><br>
-        
+
           <a href="examples/Form_Points_and_Lines.php">Points/Lines</a><br>
-          
-        
+
+
           <a href="examples/Form_Shape_Primitives.php">Shape Primitives</a><br>
-          
-        
+
+
           <a href="examples/Form_Pie_Chart.php">Pie Chart</a><br>
-          
-        
+
+
           <a href="examples/Form_Regular_Polygon.php">Regular Polygon</a><br>
-          
-        
+
+
           <a href="examples/Form_Star.php">Star</a><br>
-          
-        
+
+
           <a href="examples/Form_Triangle_Strip.php">Triangle Strip</a><br>
-          
-        
+
+
           <a href="examples/Form_Bezier.php">Bezier</a><br>
-          
-        
+
+
         </p>
-        
-      
+
+
         <p>
         <strong>Data</strong><br>
-        
+
           <a href="examples/Data_Variables.php">Variables</a><br>
-          
-        
+
+
           <a href="examples/Data_True_and_False.php">True/False</a><br>
-          
-        
+
+
           <a href="examples/Data_Variable_Scope.php">Variable Scope</a><br>
-          
-        
+
+
         </p>
-        
-      
+
+
         <p>
         <strong>Arrays</strong><br>
-        
+
           <a href="examples/Arrays_Array.php">Array</a><br>
-          
-        
+
+
           <a href="examples/Arrays_Array_2D.php">Array 2D</a><br>
-          
-        
+
+
           <a href="examples/Arrays_Array_Objects.php">Array Objects</a><br>
-          
-        
+
+
         </p>
-        
-      
+
+
         <p>
         <strong>Control</strong><br>
-        
+
           <a href="examples/Control_Iteration.php">Iteration</a><br>
-          
-        
+
+
           <a href="examples/Control_Embedded_Iteration.php">Embedded Iteration</a><br>
-          
-        
+
+
           <a href="examples/Control_Conditionals_1.php">Conditionals 1</a><br>
-          
-        
+
+
           <a href="examples/Control_Conditionals_2.php">Conditionals 2</a><br>
-          
-        
+
+
           <a href="examples/Control_Logical_Operators.php">Logical Operators</a><br>
-          
-        
+
+
         </p>
-        
-      
+
+
         <p>
         <strong>Image</strong><br>
-        
+
           <a href="examples/Image_Load_and_Display_Image.php">Load/Display Image</a><br>
-          
-        
+
+
           <a href="examples/Image_Background_Image.php">Background Image</a><br>
-          
-        
+
+
           <a href="examples/Image_Transparency.php">Transparency</a><br>
-          
-        
+
+
           <a href="examples/Image_Alpha_Mask.php">Alpha Mask</a><br>
-          
-        
+
+
           <a href="examples/Image_Create_Image.php">Create Image</a><br>
-          
-        
+
+
           <a href="examples/Image_Pointillism.php">Pointillism</a><br>
-          
-        
+
+
         </p>
-        
-      
+
+
         <p>
         <strong>Color</strong><br>
-        
+
           <a href="examples/Color_Hue.php">Hue</a><br>
-          
-        
+
+
           <a href="examples/Color_Saturation.php">Saturation</a><br>
-          
-        
+
+
           <a href="examples/Color_Brightness.php">Brightness</a><br>
-          
-        
+
+
           <a href="examples/Color_Color_Variables.php">Color Variables</a><br>
-          
-        
+
+
           <a href="examples/Color_Relativity.php">Relativity</a><br>
-          
-        
+
+
           <a href="examples/Color_Linear_Gradient.php">Linear Gradient</a><br>
-          
-        
+
+
           <a href="examples/Color_Radial_Gradient.php">Radial Gradient</a><br>
-          
-        
+
+
         </p>
-        
+
           </div>
           <div class="column_1">
-        
-      
+
+
         <p>
         <strong>Math</strong><br>
-        
+
           <a href="examples/Math_Increment_Decrement.php">Increment Decrement</a><br>
-          
-        
+
+
           <a href="examples/Math_Operator_Precedence_.php">Operator Precedence </a><br>
-          
-        
+
+
           <a href="examples/Math_Distance_1D.php">Distance 1D</a><br>
-          
-        
+
+
           <a href="examples/Math_Distance_2D.php">Distance 2D</a><br>
-          
-        
+
+
           <a href="examples/Math_Sine.php">Sine</a><br>
-          
-        
+
+
           <a href="examples/Math_Sine_Cosine.php">Sine Cosine</a><br>
-          
-        
+
+
           <a href="examples/Math_Sine_Wave.php">Sine Wave</a><br>
-          
-        
+
+
           <a href="examples/Math_Additive_Wave.php">Additive Wave</a><br>
-          
-        
+
+
           <a href="examples/Math_PolarToCartesian.php">PolarToCartesian</a><br>
-          
-        
+
+
           <a href="examples/Math_Double_Random.php">Double Random</a><br>
-          
-        
+
+
           <a href="examples/Math_Random.php">Random</a><br>
-          
-        
+
+
           <a href="examples/Math__Noise1D.php"> Noise1D</a><br>
-          
-        
+
+
           <a href="examples/Math_Noise_Wave.php">Noise Wave</a><br>
-          
-        
+
+
         </p>
-        
-      
+
+
         <p>
         <strong>Simulate</strong><br>
-        
+
           <a href="examples/Simulate_Forces.php">Forces</a><br>
-          
-        
+
+
           <a href="examples/Simulate_Particle_System.php">Particle System</a><br>
-          
-        
+
+
           <a href="examples/Simulate_Flocking.php">Flocking</a><br>
-          
-        
+
+
           <a href="examples/Simulate_Wolfram_CA.php">Wolfram CA</a><br>
-          
-        
+
+
           <a href="examples/Simulate_Game_of_Life.php">Game of Life</a><br>
-          
-        
+
+
         </p>
-        
-      
+
+
         <p>
         <strong>Objects</strong><br>
-        
+
           <a href="examples/Objects_Objects.php">Objects</a><br>
-          
-        
+
+
           <a href="examples/Objects_Multiple_Objects.php">Multiple Objects</a><br>
-          
-        
+
+
           <a href="examples/Objects_Array_of_Objects.php">Array of Objects</a><br>
-          
-        
+
+
           <a href="examples/Objects_Objects_2.php">Objects 2</a><br>
-          
-        
+
+
         </p>
-        
-      
+
+
         <p>
         <strong>Instance Mode</strong><br>
-        
+
           <a href="examples/Instance_Mode_Instantiation.php">Instantiation</a><br>
-          
-        
+
+
         </p>
-        
-      
+
+
         <p>
         <strong>Dom</strong><br>
-        
+
           <a href="examples/Dom_Input_and_Button.php">Input/Button</a><br>
-          
-        
+
+
           <a href="examples/Dom_Slider.php">Slider</a><br>
-          
-        
+
+
           <a href="examples/Dom_Modifying_the_DOM.php">Modifying the DOM</a><br>
-          
-        
+
+
           <a href="examples/Dom_Video.php">Video</a><br>
-          
-        
+
+
           <a href="examples/Dom_Video_Canvas.php">Video Canvas</a><br>
-          
-        
+
+
           <a href="examples/Dom_Video_Pixels.php">Video Pixels</a><br>
-          
-        
+
+
           <a href="examples/Dom_Video_Capture.php">Video Capture</a><br>
-          
-        
+
+
           <a href="examples/Dom_Drop.php">Drop</a><br>
-          
-        
+
+
         </p>
-        
+
           </div>
           <div class="column_2">
-        
-      
+
+
         <p>
         <strong>Sound</strong><br>
-        
+
           <a href="examples/Sound_Load_and_Play_Sound.php">Load/Play Sound</a><br>
-          
-        
+
+
           <a href="examples/Sound_Preload_SoundFile.php">Preload SoundFile</a><br>
-          
-        
+
+
           <a href="examples/Sound_soundFormats.php">soundFormats</a><br>
-          
-        
+
+
           <a href="examples/Sound_Play_Mode.php">Play Mode</a><br>
-          
-        
+
+
           <a href="examples/Sound_Pan_Sound.php">Pan Sound</a><br>
-          
-        
+
+
           <a href="examples/Sound_Sound_Effect.php">Sound Effect</a><br>
-          
-        
+
+
           <a href="examples/Sound_Playback_Rate.php">Playback Rate</a><br>
-          
-        
+
+
           <a href="examples/Sound__Amplitude_Analysis.php"> Amplitude Analysis</a><br>
-          
-        
+
+
           <a href="examples/Sound_Noise_Drum_Envelope.php">Noise Drum Envelope</a><br>
-          
-        
+
+
           <a href="examples/Sound__Note_Envelope.php"> Note Envelope</a><br>
-          
-        
+
+
           <a href="examples/Sound__Amplitude_(Volume).php"> Amplitude (Volume)</a><br>
-          
-        
+
+
           <a href="examples/Sound_Oscillator_Frequency.php">Oscillator Frequency</a><br>
-          
-        
+
+
           <a href="examples/Sound_Mic_Input.php">Mic Input</a><br>
-          
-        
+
+
           <a href="examples/Sound_Frequency_Spectrum.php">Frequency Spectrum</a><br>
-          
-        
+
+
           <a href="examples/Sound_Mic_Threshold.php">Mic Threshold</a><br>
-          
-        
+
+
           <a href="examples/Sound__Filter_LowPass.php"> Filter LowPass</a><br>
-          
-        
+
+
           <a href="examples/Sound__Filter_BandPass.php"> Filter BandPass</a><br>
-          
-        
+
+
           <a href="examples/Sound__Delay.php"> Delay</a><br>
-          
-        
+
+
           <a href="examples/Sound__Reverb.php"> Reverb</a><br>
-          
-        
+
+
           <a href="examples/Sound__Convolution_Reverb.php"> Convolution Reverb</a><br>
-          
-        
+
+
           <a href="examples/Sound__Record_Save_Audio.php"> Record Save Audio</a><br>
-          
-        
+
+
           <a href="examples/Sound_Frequency_Modulation.php">Frequency Modulation</a><br>
-          
-        
+
+
           <a href="examples/Sound_Amplitude_Modulation.php">Amplitude Modulation</a><br>
-          
-        
+
+
         </p>
-        
-      
+
+
       </div>
 
       <div class="spacer"></div>
 
       <a name="demos" class="anchor"><h3>Demos</h3></a>
       <div class="column_0">
-      
+
         <p>
         <strong>Hello P5</strong><br>
-        
+
           <a href="demos/Hello_P5_Simple_Shapes.php">Simple Shapes</a><br>
-          
-        
+
+
           <a href="demos/Hello_P5_Interactivity_1.php">Interactivity 1</a><br>
-          
-        
+
+
           <a href="demos/Hello_P5_Interactivity_2.php">Interactivity 2</a><br>
-          
-        
+
+
           <a href="demos/Hello_P5_Animation.php">Animation</a><br>
-          
-        
+
+
           <a href="demos/Hello_P5_Flocking.php">Flocking</a><br>
-          
-        
+
+
           <a href="demos/Hello_P5_Weather.php">Weather</a><br>
-          
-        
+
+
           <a href="demos/Hello_P5_Drawing.php">Drawing</a><br>
-          
-        
+
+
           <a href="demos/Hello_P5_Song.php">Song</a><br>
-          
-        
+
+
         </p>
-        
+
           </div>
           <div class="column_1">
-        
-      
+
+
       </div>
       <!--<li><a href="https://github.com/processing/p5.js/tree/master/examples/learningprocessing">Learning Processing examples</a> - port of the first 10 chapters of examples from Dan Shiffman's <a href="http://www.learningprocessing.com/examples/">Learning Processing book</a>.</li>
 
