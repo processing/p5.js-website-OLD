14500e74cb4b03c5479027b0d1910df1362a824b
mayaman26 <mayaman26@gmail.com>
2015-05-30 12:47:28 -0400

    added community page, undid text expansion for get started page

--- js/scroll.js
+++ js/scroll.js
@@ -32,7 +32,7 @@ $(function() {
     $window.scroll(function() {
         if ($window.scrollTop() > offset.top) {
             $sidebar.stop().animate({
-                marginTop: $window.scrollTop() - offset.top + topPadding
+                marginTop: $window.scrollTop() //- offset.top + topPadding
             });
         } else {
             $sidebar.stop().animate({
