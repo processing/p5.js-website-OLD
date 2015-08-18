040b1aaff62e9f63d982838e5fa30360884c096c
Lauren McCarthy <laurmccarthy@gmail.com>
2015-08-18 16:46:29 -0400

    closes #150

--- copyright.php
+++ copyright.php
@@ -12,7 +12,7 @@
       <p>The p5.js library is free software; you can redistribute it and/or modify it under the 
       terms of the <a href="https://github.com/processing/p5.js/blob/master/license.txt">GNU Lesser General 
       Public License</a> as published by the Free Software Foundation, version 2.1.</p>
-      <p>The <a href='reference/'>Reference</a> for the Language and Environment are under a <a href='http://creativecommons.org/'>Creative Commons</a> license which makes it possible to reuse this content for non-commercial purposes if it is credited.</p>
+      <p>The <a href='reference/'>Reference</a> for the Language and Environment are under a <a href='http://creativecommons.org/licenses/by-nc-sa/4.0/'>Creative Commons</a> license which makes it possible to reuse this content for non-commercial purposes if it is credited.</p>
 
     </section>
     <?php include('footer.php'); ?>
