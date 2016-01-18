4f93a4af8102e6bff5d96234a34518eb4efe3c2e
andy timmons <andrewjtimmons@gmail.com>
2016-01-17 16:42:19 -0800

    Add example for directional lighting and create directory for future light examples

---
+++ examples/examples/Lights_Directional.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+
+  <?php include('../../header.php'); ?>
+  <body id="learn-page">
+
+    <?php include('../../sidebar.php'); ?>
+
+    <!-- content sections -->
+    <div class="column-span">
+      <section>
+          <p id="backlink"><a href="../#examples">< Back to Examples</a></p>
+          <h2>Directional</h2>
+          <p>Move the mouse the change the direction of the light.
+ Directional light comes from one direction and is stronger when hitting a
+ surface squarely and weaker if it hits at a a gentle angle. After hitting a
+ surface, a directional lights scatters in all directions.
+ </p>
+
+          <div id="exampleDisplay">
+            <iframe id="exampleFrame" src="example.html" ></iframe>
+            <div class="edit_space">
+              <button id="runButton" class="edit_button">run</button>
+              <button id="resetButton" class="reset_button">reset</button>
+            </div>
+            <div id="exampleEditor"></div>
+          </div>
+
+          <p><a style="border-bottom:none !important;" href="http://creativecommons.org/licenses/by-nc-sa/4.0/" target=_blank><img src="http://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" style="width:88px"/></a></p>
+      </section>
+
+      <?php include('../../footer.php'); ?>
+    </div><!-- end column-span -->
+
+    <!-- outside of column for footer to go across both -->
+
+    <p class="clearfix"> &nbsp; </p>
+
+    <object type="image/svg+xml" data="../../img/thick-asterisk-alone.svg" id="asterisk-design-element">
+         *<!-- to do: add fallback image in CSS -->
+    </object>
+
+    <?php include('../../end.php'); ?>
+
+    <script src="../../js/vendor/ace-nc/ace.js"></script>
+    <script src="../../js/examples.js"></script>
+    <script>
+      $(document).ready( function () {
+          examples.init('../examples_src/12_Lights/02_Directional.js');
+      });
+    </script>
+  </body>
+</html>
