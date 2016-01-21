4ecd19b990e8c2107d1c4a3d40c71e2bef743384
andy timmons <andrewjtimmons@gmail.com>
2016-01-21 07:27:12 -0800

    fix spacing

--- examples/examples_src/12_Lights/05_Mixture.js
+++ examples/examples_src/12_Lights/05_Mixture.js
@@ -12,16 +12,13 @@ function draw() {
 	background(0);
 
 	// Orange point light on the right
-	pointLight(150, 100, 0, // Color
-						 500, 0, 200); // Position
+	pointLight(150, 100, 0, 500, 0, 200);
 
 	// Blue directional light from the left
-	directionalLight(0, 102, 255,  // Color
-									 -1, 0, 0); // The x-, y-, z-axis direction
+	directionalLight(0, 102, 255, -1, 0, 0);
 
 	// Yellow spotlight from the front
-	pointLight(255, 255, 109, // Color
-			  		 0, 0, 300) // Position
+	pointLight(255, 255, 109, 0, 0, 300);
 
 	rotateY(map(mouseX, 0, width, 0, PI));
 	rotateX(map(mouseY, 0, height, 0, PI));
