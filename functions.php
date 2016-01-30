60038ce7ac1aad7485c2ad0ef63003dbb67e7934
Atul Varma <varmaa@gmail.com>
2016-01-30 08:13:37 -0500

    Allow site root to be configured via env variable.

--- functions.php
+++ functions.php
@@ -2,6 +2,9 @@
 
 function getRoot() {
 
+  if (strlen(getenv('PHP_P5_SITE_ROOT')) > 0) {
+    return getenv('PHP_P5_SITE_ROOT');
+  }
   if (strpos(getenv('HTTP_HOST'), 'localhost') !== FALSE){
     return 'http://'.getenv('HTTP_HOST').'/p5js.org/';
   } else return 'http://p5js.org/';
