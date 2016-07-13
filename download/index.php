48c4ee03ab90853de9b081fa8aef6927179284cf
Lauren McCarthy <laurmccarthy@gmail.com>
2016-07-13 12:36:44 +1000

    updating ref

--- download/index.php
+++ download/index.php
@@ -21,7 +21,7 @@
         <path stroke-miterlimit="10" d="M16.909,10.259l8.533-2.576l1.676,5.156l-8.498,2.899l5.275,7.48
             l-4.447,3.225l-5.553-7.348L8.487,26.25l-4.318-3.289l5.275-7.223L0.88,12.647l1.678-5.16l8.598,2.771V1.364h5.754V10.259z"/>
         </svg>
-        <p>Includes:<br>p5.js, p5.dom.js, p5.sound.js, and an example project
+        <p>Includes:<br>p5.js, p5.dom.js, p5.sound.js, offline reference, example project
         <br>Version <?php echo $version; ?> (<?php echo $date; ?>)</p>
         </div>
         </a>
