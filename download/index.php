0d47218690ba9ab035e244a70e4af938332e5269
Lauren McCarthy <laurmccarthy@gmail.com>
2016-07-17 08:27:59 +1000

    Update index.php

--- download/index.php
+++ download/index.php
@@ -13,7 +13,7 @@
         
         <p>Processing is open source, free software. All donations fund the <a href="http://foundation.processing.org">Processing Foundation</a>,
         a nonprofit organization devoted to advancing the role of programming within the visual arts through developing Processing.<br>
-        <a href="https://processingfoundation.org/support">DONATE HERE</a>
+        <a href="https://processingfoundation.org/support">Donate here.</a>
         </p>
         
         <h3>Complete Library</h3>
